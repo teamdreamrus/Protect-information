2509b33a34c71320b0038bc60e91ab7590a0ddf2
AlexeyTOP1 <atazhanov.biz@yandex.ru>
2019-10-17 14:38:09 +0700

    add read/write files el-gamal

--- src/main/java/LGamal.java
+++ src/main/java/LGamal.java
@@ -1,12 +1,33 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 public class LGamal {
     private long A;
     private long B;
-    public LGamal(){
+    private InputStream inputStream;
+    private OutputStream outputStream;
+    public long fileSize;
+
+    public LGamal() throws IOException {
+
         DiffHellman Diff =new DiffHellman();
 //        Diff.P=23;
 //        Diff.g=5;
+        String path = "smile.jpg";
+        inputStream = new FileInputStream(path);
+        outputStream = new FileOutputStream("NEW"+path);
+        File file = new File(path);
+        fileSize = file.length();
+        byte[] buffer = new byte[(int)fileSize];
+        inputStream.read(buffer, 0, buffer.length);
+        int[] bytes = new int[buffer.length];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = buffer[i] & 0xFF;
 
-
+            //vse okay
+        }
         Man Alisa = new Man(Diff.P,Diff.g);
         Man Bob = new Man(Diff.P,Diff.g);
 //        Bob.closedKey = 13;
@@ -14,13 +35,48 @@ public class LGamal {
 //        Alisa.sessionKey=7;
         Mod m1 = new Mod(Diff.g,Alisa.getSessionKey(),Diff.P);
         A=m1.exp2().longValue();
-        int message = 1555;
-        Mod m2= new Mod(Bob.openKey,Alisa.getSessionKey(),Diff.P);
-        B=message*m2.exp2().longValue() % Diff.P;
+        FileWriter writer1 = new FileWriter("Temp.txt", true);
+        writer1.write(A + " ");
+        writer1.flush();
+        for(int message : bytes) {
+            //   int message = 1555;
+            Mod m2 = new Mod(Bob.openKey, Alisa.getSessionKey(), Diff.P);
+            B = message * m2.exp2().longValue() % Diff.P;
+            FileWriter writer = new FileWriter("Temp.txt", true);
+            writer.write( B + " ");
+            writer.flush();
+        }
+        String fileName = "Temp.txt";
+        String content = Files.lines(Paths.get(fileName)).reduce("", String::concat);
+        String[] koded = content.split(" ");
+        long newA=Long.parseLong(koded[0]);
+        ArrayList<Integer> kodedInt= new ArrayList<>();
+        boolean x=false;
+        for(String el : koded){
+            if(x)
+            kodedInt.add(Integer.valueOf(el));
+            x=true;
+           // System.out.println(el);
+        }
+        byte[] buffer1 = new byte[(int)fileSize];
+        for (int i = 0; i < bytes.length; i++) {
+            Mod m3 = new Mod(newA, Diff.P - 1 - Bob.getClosedKey(), Diff.P);
+            long messageNew = kodedInt.get(i) * m3.exp2().longValue() % Diff.P;
+            buffer1[i]=(byte)(int)messageNew;
+            System.out.println(messageNew);
+        }
+        outputStream.write(buffer1, 0, buffer1.length);
+    }
+    public class InputOutputStreamExam {
+        private InputStream inputStream;
+        private OutputStream outputStream;
+        private String path;
 
-        Mod m3= new Mod(A,Diff.P-1-Bob.getClosedKey(),Diff.P);
-        long messageNew =B*m3.exp2().longValue() % Diff.P;
-        System.out.println(messageNew);
+
+        public InputOutputStreamExam(String path) {
+            this.path = path;
+
+        }
 
     }
 }
--- src/main/java/Vernam.java
+++ src/main/java/Vernam.java
@@ -1,42 +1,87 @@
 import sun.awt.windows.WPrinterJob;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Vernam {
-    public Vernam() {
-        String message = "Hello world!";
-        char[] achText = message.toCharArray();
+    private InputStream inputStream;
+    private OutputStream outputStream;
+    public long fileSize;
+    public Vernam() throws IOException {
+      //  String message = "Hello world!";
+        String path = "smile.jpg";
+        inputStream = new FileInputStream(path);
+        outputStream = new FileOutputStream("NEW"+path);
+        File file = new File(path);
+        fileSize = file.length();
+        byte[] buffer = new byte[(int)fileSize];
+        inputStream.read(buffer, 0, buffer.length);
+        int[] bytes = new int[buffer.length];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = buffer[i] & 0xFF;
+
+            //vse okay
+        }
+
+      //  char[] achText = message.toCharArray();
         char[] achKey = new char[achText.length];
         char[] achResult = new char[achText.length];
         Random random = new Random();
-        for (int i = 0; i < achText.length; i++) {
-            achKey[i] = (char) random.nextInt(Character.MAX_VALUE);
-            achResult[i] = (char) (achText[i] ^ achKey[i]);
+        for (int i = 0; i < (int)fileSize; i++) {
+            achKey[i] = (char) random.nextInt(Byte.MAX_VALUE);
+            achResult[i] = (char) (bytes[i] ^ achKey[i]);
+            FileWriter writer = new FileWriter("Temp.txt", true);
+            writer.write(achResult[i]+" ");
+            writer.flush();
         }
-        System.out.print("Text: ");
-        for (char a : achText) {
-            System.out.print((int) a+" ");
+        String fileName = "Temp.txt";
+        String content = Files.lines(Paths.get(fileName)).reduce("", String::concat);
+        String[] koded = content.split(" ");
+        ArrayList<Integer> kodedInt= new ArrayList<>();
+        for(String el : koded){
+            kodedInt.add(Integer.valueOf(el));
+            System.out.println(el);
         }
-        System.out.println();
-        System.out.print("Key: ");
-        for (char a : achKey) {
-            System.out.print((int) a+" ");
-        }
-        System.out.println();
-        System.out.print("Result: ");
-        for (char a : achResult) {
-            System.out.print((int) a+" ");
-        }
-        System.out.println();
+//        System.out.print("Text: ");
+//        for (char a : achText) {
+//            System.out.print((int) a+" ");
+//        }
+//        System.out.println();
+//        System.out.print("Key: ");
+//        for (char a : achKey) {
+//            System.out.print((int) a+" ");
+//        }
+//        System.out.println();
+//        System.out.print("Result: ");
+//        for (char a : achResult) {
+//            System.out.print((int) a+" ");
+//        }
+//        System.out.println();
 
 
       //  System.out.println("Result: " + String.valueOf(achResult));
-
+        byte[] buffer1 = new byte[(int)fileSize];
         char[] achDecrypt = new char[achText.length];
-        for (int i = 0; i < achText.length; i++) {
-            achDecrypt[i] = (char) (achResult[i] ^ achKey[i]);
+        for (int i = 0; i < kodedInt.size(); i++) {
+          //  achDecrypt[i] = ;
+            buffer1[i]=(byte)(int)(kodedInt.get(i) ^ achKey[i]);
         }
 
-        System.out.println("Decrypt: " + String.valueOf(achDecrypt));
+        outputStream.write(buffer1, 0, buffer1.length);
+    }
+    public class InputOutputStreamExam {
+        private InputStream inputStream;
+        private OutputStream outputStream;
+        private String path;
+
+
+        public InputOutputStreamExam(String path) {
+            this.path = path;
+
+        }
+
     }
 }
